b07e35c8378830f797ec55e8414098ca4b208fb1
Baptiste Fontaine <batifon@yahoo.fr>
2014-10-14 09:10:26 +0200

    CLI tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,12 +1,72 @@
 # -*- coding: UTF-8 -*-
 
-try:
-    import unittest2 as unittest
-except ImportError:
-    import unittest
+import sys
+
+import responses
+
+from helpers import TestCase
 
 from wptranslate.cli import main
 
-class TestCli(unittest.TestCase):
+class TestCli(TestCase):
 
-    pass  # TODO
+    def setUp(self):
+        self.sys_argv = sys.argv
+        self.sys_exit = sys.exit
+        self.exit_code = None
+        def _fake_exit(code=None):
+            self.exit_code = code
+        _fake_exit.__name__ = sys.exit.__name__
+        sys.exit = _fake_exit
+        self.lang = 'foo'
+        self.url = 'https://%s.wikipedia.org/w/api.php' % self.lang
+        self.responses = []
+
+    def tearDown(self):
+        sys.argv = self.sys_argv
+        sys.exit = self.sys_exit
+
+    def add_resp(self, code, body):
+        self.responses.append((code, {}, body))
+
+    def mk_resps_callback(self):
+        return lambda r: self.responses.pop(0)
+
+
+    @responses.activate
+    def test_abort_on_keyboard_interrupt(self):
+        def ctrlC(*args, **kw):
+            raise KeyboardInterrupt()
+
+        responses.add_callback(method=responses.GET, url=self.url, callback=ctrlC)
+        sys.argv = ['wptranslate', '-s', self.lang, '-t', 'en', 'myword']
+        self.assertNone(self.exit_code)
+        main(verbose=False)
+        self.assertEquals(1, self.exit_code)
+
+    @responses.activate
+    def test_error_if_no_result(self):
+        responses.add(method=responses.GET, url=self.url, body='{}')
+        sys.argv = ['wptranslate', '-s', self.lang, '-t', 'en', 'myword']
+        self.assertNone(self.exit_code)
+        main(verbose=False)
+        self.assertEquals(1, self.exit_code)
+
+    @responses.activate
+    def test_success(self):
+        title = 'fooqz+_a123q'
+        word = 'qa2f*$&x0'
+        titlebody = '{"query": {"search":[{"title":"%s"}]}}' % title
+        pagebody = '''{"query":
+            {"pages": {"foo": {"langlinks":[{"*":"%s"}]}}}}''' % word
+        self.add_resp(200, titlebody)
+        self.add_resp(200, pagebody)
+        cb = self.mk_resps_callback()
+        responses.add_callback(responses.GET, self.url, callback=cb)
+        sys.argv = ['wptranslate', '-s', self.lang, '-t', 'en', 'myword']
+        self.assertNone(self.exit_code)
+        main(verbose=False)
+        self.assertNone(self.exit_code)
+        self.assertEquals(2, len(responses.calls))
+        self.assertEquals(titlebody, responses.calls[0].response.text)
+        self.assertEquals(pagebody, responses.calls[1].response.text)
--- wptranslate/cli.py
+++ wptranslate/cli.py
@@ -9,7 +9,7 @@ import sys
 from wptranslate import translate
 
 
-def main():
+def main(verbose=True):
     parser = argparse.ArgumentParser(description='Wikipedia-based Translator')
     parser.add_argument('word', type=str, help='word or expression to translate')
     parser.add_argument('-s', '--source', dest='source', type=str,
@@ -23,10 +23,12 @@ def main():
     try:
         res = translate(word, source=source, target=target)
     except KeyboardInterrupt:
-        pass
-
-    if res is None:
-        print('No result for "%s" from %s to %s' % (word, source, target))
         return sys.exit(1)
 
-    print(res)
+    if res is None:
+        if verbose:
+            print('No result for "%s" from %s to %s' % (word, source, target))
+        return sys.exit(1)
+
+    if verbose:
+        print(res)
